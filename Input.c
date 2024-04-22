d4ed3f9781729b06b06cf8ac0f189feb935d249f
Harsehej preet kaur <156522729+Harsehej22@users.noreply.github.com>
2024-04-22 10:43:04 +0530

    Add files via upload

---
+++ Input.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    printf("%d",x);
+     //simple interest
+    float s;
+    printf("enter a value :");
+       scanf("%f",&s);
+
+    float y;
+    printf("enter a value :");
+       scanf("%f",&y);
+    float t;
+    printf("enter a value :");
+       scanf("%f",&t);
+    printf("\n\n%f",(s*y*t)/100);
+    //sum of two numbers
+    int q;
+    int w;
+     printf("\nenter a value :");
+    scanf("%d",&w);
+        printf("enter a value :");
+        scanf("%d",&q);
+           
+            printf("\n sum is ; %d",q+w);
+    
+
+    return 0;
+
+}
